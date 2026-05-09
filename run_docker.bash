b5a7a2c56d0c4603da1808b59c28fa51c02c8180
Mark Lights <marklights899@gmail.com>
2026-05-09 17:24:33 -0400

    port

--- run_docker.bash
+++ run_docker.bash
@@ -2,4 +2,4 @@ docker rm robinhood-api
 
 docker build -t robinhood-api .
 
-docker run 8081:8081 --name robinhood-api --restart unless-stopped robinhood-api
+docker run -p 8081:8081 --name robinhood-api --restart unless-stopped robinhood-api
